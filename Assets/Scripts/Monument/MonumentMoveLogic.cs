dab9cf88081e6d89d1e06189d962bede33e52d45
Cmanchik <aksenova1648@gmail.com>
2021-06-10 12:42:18 +0300

    Поднятие и опускание монумента при помощи пластин

---
+++ Assets/Scripts/Monument/MonumentMoveLogic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Monument
+{
+    public class MonumentMoveLogic : MonoBehaviour
+    {
+        [SerializeField] private Transform startPos;
+        [SerializeField] private Transform endPos;
+
+        [Range(0f, 1f)]
+        [Space] [SerializeField] private float speed;
+        
+        public bool IsUp { get; set; }
+
+        private void FixedUpdate()
+        {
+            transform.position = Vector2.Lerp(transform.position, IsUp ? endPos.position : startPos.position, speed);
+        }
+    }
+}
---
+++ Assets/Scripts/Monument/PushPlateScript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace Monument
+{
+    public class PushPlateScript : MonoBehaviour
+    {
+        [SerializeField] private MonumentMoveLogic monumentMoveLogic;
+        private int _objectRaiseId = 0;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.layer != LayerMask.NameToLayer("Player") || _objectRaiseId != 0) return;
+            
+            _objectRaiseId = other.gameObject.GetInstanceID();
+
+        }
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            if (_objectRaiseId != 0) monumentMoveLogic.IsUp = true;
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.gameObject.GetInstanceID() != _objectRaiseId) return;
+            monumentMoveLogic.IsUp = false;
+            _objectRaiseId = 0;
+        }
+    }
+}
