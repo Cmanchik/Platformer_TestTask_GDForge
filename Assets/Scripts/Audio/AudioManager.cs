bbaf51dc965fd3bc8596c29a7ccdbd986bcb5702
Cmanchik <aksenova1648@gmail.com>
2021-06-10 23:01:41 +0300

    Добавление музыки и изменение ее громкости

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Audio
+{
+    public class AudioManager : MonoBehaviour
+    {
+        [SerializeField] private Slider volumeSlider;
+        [SerializeField] private AudioSource audioSource;
+
+        private void Start()
+        {
+            volumeSlider.onValueChanged.AddListener(delegate { ChangeVolume(); });
+        }
+
+        private void ChangeVolume()
+        {
+            audioSource.volume = volumeSlider.value;
+        }
+    }
+}
