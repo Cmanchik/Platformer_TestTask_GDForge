495a3d06daf82ed9089abfd37774c77365d9fe0d
Cmanchik <aksenova1648@gmail.com>
2021-06-10 11:49:56 +0300

    Механика смерти

---
+++ Assets/Scripts/Health/HealthScript.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Health
+{
+    public class HealthScript : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent deathEvent;
+        [SerializeField] private LayerMask deathLayer;
+
+        private void Start()
+        {
+            deathEvent.AddListener(Death);
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.layer == deathLayer)
+            {
+                deathEvent.Invoke();
+            }
+        }
+
+        private void Death()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
